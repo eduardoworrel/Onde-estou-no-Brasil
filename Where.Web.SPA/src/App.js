2d9bededc9a65add7951e9864cc5527095b6a59c
eduardoworrel <=>
2022-01-15 14:07:02 -0300

    estrutura do spa

---
+++ Where.Web.SPA/src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+import Home from './pages/Home';
+import Where from './pages/Where';
+import Erramos from './pages/Erramos';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/where" element={<Where />} />
+      <Route path="/erramos" element={<Erramos />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
