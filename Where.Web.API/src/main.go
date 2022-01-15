92c7a71ba2eee0f6143344693e4cf3fe9d935830
eduardoworrel <=>
2022-01-15 14:35:57 -0300

    cors

--- Where.Web.API/src/main.go
+++ Where.Web.API/src/main.go
@@ -51,6 +51,10 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	e.GET("/getByIP", func(c echo.Context) error {
 
